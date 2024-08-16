2b6b00209eaa04594cb8db228404a5de1f5f9c17
Büşra Göktaş <busra1gkts@gmail.com>
2024-08-16 14:50:57 +0300

    Partial view Model binding

--- MVC_Proje.Web/Controllers/HomeController.cs
+++ MVC_Proje.Web/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using MVC_Proje.Web.Models;
+using MVC_Proje.Web.ViewModels;
 using System.Diagnostics;
 
 namespace MVC_Proje.Web.Controllers
@@ -7,19 +8,47 @@ namespace MVC_Proje.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly AppDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger , AppDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
+            var products = _context.Products.OrderByDescending(x => x.Id).Select(x => new ProductPartialViewModel()
+            {
+                Id = x.Id,
+                Name = x.Name,
+                Price = x.Price,
+                Stock = x.Stock
+            }).ToList();
+
+
+            ViewBag.ProductListPartialViewModel = new ProductListPartialViewModel()
+            {
+                Products = products
+            };
             return View();
         }
 
         public IActionResult Privacy()
         {
+            var products = _context.Products.OrderByDescending(x => x.Id).Select(x => new ProductPartialViewModel()
+            {
+                Id = x.Id,
+                Name = x.Name,
+                Price = x.Price,
+                Stock = x.Stock
+            }).ToList();
+
+
+            ViewBag.ProductListPartialViewModel = new ProductListPartialViewModel()
+            {
+                Products = products
+            };
             return View();
         }
 
---
+++ MVC_Proje.Web/ViewModels/ProductListPartialViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace MVC_Proje.Web.ViewModels
+{
+    public class ProductListPartialViewModel
+    {
+        public List<ProductPartialViewModel> Products { get; set; }
+    }
+
+    public class ProductPartialViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+    }
+}
