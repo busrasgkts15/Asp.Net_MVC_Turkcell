957891353d91a2255f719faf0c9c3e24afac2409
Büşra Göktaş <busra1gkts@gmail.com>
2024-08-27 20:11:16 +0300

    Visitor component part 5 completed

--- MVC_Proje.Web/Views/Shared/ViewComponents/VisitorViewComponent.cs
+++ MVC_Proje.Web/Views/Shared/ViewComponents/VisitorViewComponent.cs
@@ -1,12 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using MVC_Proje.Web.Models;
+using MVC_Proje.Web.ViewModels;
 
 namespace MVC_Proje.Web.Views.Shared.ViewComponents
 {
     public class VisitorViewComponent:ViewComponent
     {
+        private readonly AppDbContext _context;
+        private readonly IMapper _mapper;
+
+
+        public VisitorViewComponent(AppDbContext context, IMapper mapper)
+        {
+            _mapper = mapper;
+            _context = context;
+        }
 
         public async Task<IViewComponentResult> InvokeAsync()
         {
+
+            var visitors = _context.Visitors.ToList();
+
+            var visitorViewModel = _mapper.Map<List<VisitorViewModel>>(visitors);
+            ViewBag.visitorViewModel = visitorViewModel;
+
             return View();
         }
     }
